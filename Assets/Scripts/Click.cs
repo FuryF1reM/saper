6e419d1f8d6d80157a7789f8d7e5218c15ff428f
Serhiy <zemvan3310@gmail.com>
2026-03-19 21:49:42 +0200

    FinalUpdate
    Menu GameSettings.cs Click.cs GridManager.cs

--- Assets/Scripts/Click.cs
+++ Assets/Scripts/Click.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
+using TMPro;
 public class Click : MonoBehaviour
 {
     public GameObject mainPanel;
     public GameObject creditsPanel;
     public GameObject playPanel;
     public GameObject customPanel;
+    [Header("Custom Input")]
+    public TMP_InputField gridSizeInput;
+    public TMP_InputField mineCountInput;
     void Start()
     {
         mainPanel.SetActive(true);
@@ -44,5 +48,48 @@ public class Click : MonoBehaviour
     {
         playPanel.SetActive(true);
         customPanel.SetActive(false);
+    }
+    public void SetEasy()
+    {
+        GameSettings.gridSize = 10;
+        GameSettings.mineCount = 10;
+
+        SceneManager.LoadScene("saper"); 
+    }
+
+    public void SetMedium()
+    {
+        GameSettings.gridSize = 17;
+        GameSettings.mineCount = 40;
+
+        SceneManager.LoadScene("saper");
+    }
+
+    public void SetHard()
+    {
+        GameSettings.gridSize = 22;
+        GameSettings.mineCount = 99;
+
+        SceneManager.LoadScene("saper");
+    }
+    public void StartCustomGame()
+    { 
+        int size;
+        int mines;
+
+        if (!int.TryParse(gridSizeInput.text, out size)) return;
+        if (!int.TryParse(mineCountInput.text, out mines)) return;
+
+        if (size < 2) size = 2;
+
+        int maxMines = size * size - 1;
+
+        if (mines < 1) mines = 1;
+        if (mines > maxMines) mines = maxMines;
+
+        GameSettings.gridSize = size;
+        GameSettings.mineCount = mines;
+
+        SceneManager.LoadScene("saper");
     }
 }
---
+++ Assets/Scripts/GameSettings.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+public static class GameSettings
+{
+    public static int gridSize = 10;
+    public static int mineCount = 10;
+}
